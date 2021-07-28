97a2c73eb276414d2ec6aec44fed881153a89081
0vishal <vishalsalaskar72@gmail.com>
2021-07-28 15:07:37 +0530

    [Vishal] Delete : unwanted files

--- Apache Kakfa/log.py
+++
@@ -1,9 +0,0 @@
-import logging,os
-
-def logger(text):
-    
-    log=logging.getLogger(__name__)
-    logging.basicConfig(filename='logging.log',level=logging.INFO,format='%(levelname)s :%(name)s: %(asctime)s: %(message)s')
-    log.info(text)
- 
-logger("test")    
